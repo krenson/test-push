67dfd11e6af29cf44f0e80cfce2c5abffbd0c57d
pdietger <dietger.pieters@inetum-realdolmen.world>
2022-07-25 18:17:47 +0200

    IMR-21: Added extra logging [skip-ci]

--- core/src/main/java/com/leforemhe/aem/site/core/services/ContentFragmentUtilService.java
+++ core/src/main/java/com/leforemhe/aem/site/core/services/ContentFragmentUtilService.java
@@ -2,6 +2,7 @@ package com.leforemhe.aem.site.core.services;
 
 import com.adobe.cq.dam.cfm.ContentElement;
 import com.adobe.cq.dam.cfm.ContentFragment;
+import com.sun.security.auth.module.LdapLoginModule;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
@@ -19,7 +20,7 @@ import java.util.*;
 public class ContentFragmentUtilService {
 
     public static final String ELEMENT_TITLE = "titre";
-    
+
     @Reference
     private ResourceResolverFactory resolverFactory;
 
@@ -39,12 +40,13 @@ public class ContentFragmentUtilService {
     public Map<String, Object> getContentFromContentFragment(String contentFragmentId, String contentFragmentModel, String[] contentList) {
         ResourceResolver resourceResolver = ServiceUtils.getResourceResolver(resolverFactory, globalConfigService.getConfig().systemUser());
         Map<String, Object> contentFragmentData;
-        LOG.debug("Inside get content from content fragment.");
+        LOG.debug("Inside get content from content fragment for ID {}.", contentFragmentId);
         LOG.debug("System user: {}", globalConfigService.getConfig().systemUser());
         LOG.debug("Resource resolver factory {}.", resolverFactory);
         LOG.debug("Resource resolver {}.", resourceResolver);
         LOG.debug("Content fragment model {}.", contentFragmentModel);
         Iterator<Resource> damContentFragments = resourceResolver.getResource(contentFragmentModel).listChildren();
+        LOG.debug("Parent content fragment folder: {}.", damContentFragments);
         List<ContentFragment> listActivities = getContentFragmentsWithKeyProfession(damContentFragments, contentFragmentId);
 
 
@@ -55,18 +57,23 @@ public class ContentFragmentUtilService {
 
     private List<ContentFragment> getContentFragmentsWithKeyProfession(Iterator<Resource> initialList, String keyProfession) {
         List<ContentFragment> contentFragments = new ArrayList<ContentFragment>();
+        LOG.debug("Inside get content from content fragment with ID.");
+
 
         if (initialList != null) {
             do {
 
                 Resource resource = initialList.next();
                 if (resource.getResourceType().equals("dam:Asset")) {
+                    LOG.debug("Content fragment found in metier folder");
+
                     ContentFragment contentFragment = resource.adaptTo(ContentFragment.class);
 
                     Iterator<ContentElement> elementsIt = contentFragment.getElements();
                     do {
                         ContentElement contentElement = elementsIt.next();
                         if (contentElement.getName().equals("codeMetier") && contentElement.getValue().getValue().equals(keyProfession)) {
+                            LOG.debug("Content fragment found in metier folder with same key");
                             contentFragments.add(contentFragment);
                         }
                     } while (elementsIt.hasNext());
@@ -81,6 +88,7 @@ public class ContentFragmentUtilService {
         Map<String, Object> contentFragmentData = new HashMap<>();
 
             for (String key : contentList) {
+                LOG.debug("Getting {} from contentfragment from element {}", key, contentFragment.getElement(key));
                 contentFragmentData.put(key, contentFragment.getElement(key).getValue().getValue());
             }
 
