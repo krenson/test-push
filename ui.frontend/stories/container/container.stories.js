7a3727122db0d85fac59737b3bc8472d5a8fcf2d
Stephane Martin <stephane.martin@inetum-realdolmen.world>
2022-07-29 15:26:32 +0200

    IMR57-Finish Template Metier [skip-ci]

--- ui.frontend/stories/container/container.stories.js
+++ ui.frontend/stories/container/container.stories.js
@@ -1,5 +1,8 @@
 import container from "./container.html";
 import page2 from "./page2.html";
+import page3 from "./page3.html";
+import page4 from "./page4.html";
+import page5 from "./page5.html";
 import { storiesOf } from "@storybook/html";
 
 storiesOf("AEM Components/Container", container).add(
@@ -7,3 +10,6 @@ storiesOf("AEM Components/Container", container).add(
   () => container
 );
 storiesOf("AEM Components/Container", page2).add("Page 2", () => page2);
+storiesOf("AEM Components/Container", page3).add("Page 3", () => page3);
+storiesOf("AEM Components/Container", page4).add("Page 4", () => page4);
+storiesOf("AEM Components/Container", page5).add("Page 5", () => page5);
