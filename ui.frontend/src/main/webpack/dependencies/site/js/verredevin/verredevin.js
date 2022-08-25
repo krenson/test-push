d1d1fa41821bba0c2201b27ae8b30e4f524fcfc3
H. Pacifique Bayisenge <hpacifique@live.be>
2022-08-25 11:06:59 +0200

    IMR-65-verredevin-correction [skip-ci]

---
+++ ui.frontend/src/main/webpack/dependencies/site/js/verredevin/verredevin.js
@@ -0,0 +1,11 @@
+var detailsBtn = document.getElementById(
+  "accordion-2098845c33-item-ace86c638b-button"
+);
+
+var detailsCloseBtn = document.getElementById("close-container");
+
+detailsCloseBtn.addEventListener("click", () => {
+  detailsBtn.click();
+});
+
+detailsBtn.click();
