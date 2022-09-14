18eb9f9e011bbbe79be8949705962a2c7f511d30
pdietger <dietger.pieters@inetum-realdolmen.world>
2022-09-14 17:49:38 +0200

    IMR-88: Remove old code [skip-ci]

--- core/src/main/java/com/leforemhe/aem/site/core/models/horizonemploi/Synonymes.java
+++
@@ -1,9 +0,0 @@
-package com.leforemhe.aem.site.core.models.horizonemploi;
-
-import com.adobe.cq.wcm.core.components.models.List;
-import org.osgi.annotation.versioning.ProviderType;
-
-@ProviderType
-public interface Synonymes extends List {
-    String[] getSynonymes();
-}
