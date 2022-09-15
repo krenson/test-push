dc0e5f164b6b6360dbc6b8035130fea1eb1142a9
H. Pacifique Bayisenge <hpacifique@live.be>
2022-09-15 13:25:22 +0200

    IMR-81-Onglet-Liens-Frontend [skip-ci]

--- ui.frontend/stories/links/links.stories.js
+++ ui.frontend/stories/links/links.stories.js
@@ -1,4 +1,7 @@
 import links from "./links.html";
+import links2 from "./links2.html";
 import { storiesOf } from "@storybook/html";
 
-storiesOf("AEM Components", links).add("Links", () => links);
+storiesOf("AEM Components/Links", links).add("Links", () => links);
+
+storiesOf("AEM Components/Links", links2).add("Links Onglet", () => links2);
