d24508d8da8869be7c68ffe83179cdc1c2881714
H. Pacifique Bayisenge <hpacifique@live.be>
2022-09-15 14:18:21 +0200

    IMR-58-Metiers-Proches-Frontend [skip-ci]

---
+++ ui.frontend/stories/metiersProches/metiersProches.stories.js
@@ -0,0 +1,7 @@
+import metiersProches from "./metiersProches.html";
+import { storiesOf } from "@storybook/html";
+
+storiesOf("AEM Components", metiersProches).add(
+  "Metiers Proches",
+  () => metiersProches
+);
