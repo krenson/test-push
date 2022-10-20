2dbc1490f31cfbc016217d928434ee9e0100e836
H. Pacifique Bayisenge <hpacifique@live.be>
2022-10-20 14:48:05 +0200

    IMR-95-Icons-too-big-correction [skip-ci]

--- ui.frontend/stories/LinkIcone/linkIcone.stories.js
+++ ui.frontend/stories/LinkIcone/linkIcone.stories.js
@@ -1,4 +1,9 @@
-import linkIcone from "./linkIcone.html";
+import teaser from "./teaser.html";
+import navigation from "./navigation.html";
 import { storiesOf } from "@storybook/html";
 
-storiesOf("AEM Components", linkIcone).add("Link Icone", () => linkIcone);
+storiesOf("AEM Components/LinkIcone", navigation).add(
+  "Navigation",
+  () => navigation
+);
+storiesOf("AEM Components/LinkIcone", teaser).add("Teaser", () => teaser);
