b6937e643e017cb20ae9eb084e28f2273a7ef3de
H. Pacifique Bayisenge <hpacifique@live.be>
2022-10-24 12:03:40 +0200

    IMR-140-searchbox-form

--- ui.frontend/stories/rechercheMetier/rechercheMetier.stories.js
+++ ui.frontend/stories/rechercheMetier/rechercheMetier.stories.js
@@ -1,7 +1,13 @@
 import rechercheMetier from "./rechercheMetier.html";
+import rechercheMetierForm from "./rechercheMetierForm.html";
 import { storiesOf } from "@storybook/html";
 
-storiesOf("AEM Components", rechercheMetier).add(
+storiesOf("AEM Components/Recherche Metier", rechercheMetier).add(
   "Recherche Metier",
   () => rechercheMetier
+);
+
+storiesOf("AEM Components/Recherche Metier", rechercheMetierForm).add(
+  "Recherche Metier Form",
+  () => rechercheMetierForm
 );
