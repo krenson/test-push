eee28476e9e79fa15961ec9598f0163483e9fbd6
H. Pacifique Bayisenge <hpacifique@live.be>
2022-10-25 16:53:08 +0200

    IMR-171 extra styling search component  [skip-ci]

--- ui.frontend/stories/rechercheMetier/rechercheMetier.stories.js
+++ ui.frontend/stories/rechercheMetier/rechercheMetier.stories.js
@@ -1,10 +1,16 @@
 import rechercheMetier from "./rechercheMetier.html";
+import rechercheMetier2 from "./rechercheMetier2.html";
 import rechercheMetierForm from "./rechercheMetierForm.html";
 import { storiesOf } from "@storybook/html";
 
 storiesOf("AEM Components/Recherche Metier", rechercheMetier).add(
   "Recherche Metier",
   () => rechercheMetier
+);
+
+storiesOf("AEM Components/Recherche Metier", rechercheMetier2).add(
+  "Recherche Metier 2",
+  () => rechercheMetier2
 );
 
 storiesOf("AEM Components/Recherche Metier", rechercheMetierForm).add(
