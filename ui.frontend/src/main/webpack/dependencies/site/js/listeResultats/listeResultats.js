0c12c31c340b9b1b38259ddc4502c16e9fdc9a6d
H. Pacifique Bayisenge <hpacifique@live.be>
2022-10-25 17:16:08 +0200

    IMR-154 js fix   [skip-ci]

--- ui.frontend/src/main/webpack/dependencies/site/js/listeResultats/listeResultats.js
+++ ui.frontend/src/main/webpack/dependencies/site/js/listeResultats/listeResultats.js
@@ -2,7 +2,7 @@ const searchBtn = document.querySelector(".aem.searchBtn button");
 const resultcontainer = document.querySelector(".result-counter ");
 const resultLabel = document.querySelector(".result-counter h5");
 
-const searchBox = document.querySelector(".searchContainer");
+const searchContainer = document.querySelector(".searchContainer");
 const filAriane = document.querySelector("#filAriane");
 const titleContainer = document.querySelector(".aem.title-container");
 const title = document.querySelector(".aem.title-container h1");
@@ -12,11 +12,11 @@ searchBtn.addEventListener("click", () => {
   const rdmNr = Math.floor(Math.random() * (10 - 0 + 1) + 0);
 
   if (window.screen.width <= 1200) {
-    searchBox.style.display = "none";
+    searchContainer.style.display = "none";
     filAriane.style.display = "block";
     titleContainer.style.alignItems = "start";
-    title.innerText = "Résultats";
 
+    title.innerText = "Résultats";
     subtitle.innerText = rdmNr + " métiers trouvés";
     if (rdmNr == 1) subtitle.innerText = rdmNr + " métier trouvé";
 
@@ -31,9 +31,10 @@ searchBtn.addEventListener("click", () => {
 });
 
 filAriane.addEventListener("click", () => {
-  searchBox.style.display = "flex";
+  searchContainer.style.display = "flex";
   filAriane.style.display = "none";
   titleContainer.style.alignItems = "center";
+
   title.innerText = "Rechercher des informations sur un métier";
   subtitle.innerText =
     "Un catalogue de près de 650 métiers pour vous aider à trouver votre voie.";
