a345d85d81d9101f37be36c4a02c03a059c8aacb
H. Pacifique Bayisenge <hpacifique@live.be>
2022-10-25 17:17:46 +0200

    IMR-154 js fix   [skip-ci]

--- ui.frontend/src/main/webpack/dependencies/site/js/listeResultats/listeResultats.js
+++ ui.frontend/src/main/webpack/dependencies/site/js/listeResultats/listeResultats.js
@@ -9,7 +9,7 @@ const title = document.querySelector(".aem.title-container h1");
 const subtitle = document.querySelector(".aem.title-container h5.subTitle");
 
 searchBtn.addEventListener("click", () => {
-  const rdmNr = Math.floor(Math.random() * (10 - 0 + 1) + 0);
+  const randomSearchResult = Math.floor(Math.random() * (10 - 0 + 1) + 0);
 
   if (window.screen.width <= 1200) {
     searchContainer.style.display = "none";
@@ -17,17 +17,19 @@ searchBtn.addEventListener("click", () => {
     titleContainer.style.alignItems = "start";
 
     title.innerText = "Résultats";
-    subtitle.innerText = rdmNr + " métiers trouvés";
-    if (rdmNr == 1) subtitle.innerText = rdmNr + " métier trouvé";
+    subtitle.innerText = randomSearchResult + " métiers trouvés";
+    if (randomSearchResult == 1)
+      subtitle.innerText = randomSearchResult + " métier trouvé";
 
     return;
   }
 
   resultcontainer.style.display = "flex";
 
-  resultLabel.innerText = rdmNr + " métiers trouvés";
+  resultLabel.innerText = randomSearchResult + " métiers trouvés";
 
-  if (rdmNr == 1) resultLabel.innerText = rdmNr + " métier trouvé";
+  if (randomSearchResult == 1)
+    resultLabel.innerText = randomSearchResult + " métier trouvé";
 });
 
 filAriane.addEventListener("click", () => {
