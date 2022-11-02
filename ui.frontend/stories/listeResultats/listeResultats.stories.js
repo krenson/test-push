b62b8744ae258cefb0fb2f5221d71cd5b048c271
H. Pacifique Bayisenge <hpacifique@live.be>
2022-11-02 15:14:26 +0100

    IMR-154 js issue  [skip-ci]

--- ui.frontend/stories/listeResultats/listeResultats.stories.js
+++ ui.frontend/stories/listeResultats/listeResultats.stories.js
@@ -1,19 +1,25 @@
+import listeResultats from "./listeResultats.html";
 import listeResultatsMore from "./listeResultatsMore.html";
 import listeResultatsOne from "./listeResultatsOne.html";
 import listeResultatsZero from "./listeResultatsZero.html";
 import { storiesOf } from "@storybook/html";
 
+storiesOf("AEM Components/Liste Resultats", listeResultats).add(
+  "Liste Resultats",
+  () => listeResultats
+);
+
 storiesOf("AEM Components/Liste Resultats", listeResultatsMore).add(
-  "Liste Resultats More",
+  "Liste Resultats (+99)",
   () => listeResultatsMore
 );
 
 storiesOf("AEM Components/Liste Resultats", listeResultatsOne).add(
-  "Liste Resultats One",
+  "Liste Resultats (1)",
   () => listeResultatsOne
 );
 
 storiesOf("AEM Components/Liste Resultats", listeResultatsZero).add(
-  "Liste Resultats Zero",
+  "Liste Resultats (0)",
   () => listeResultatsZero
 );
