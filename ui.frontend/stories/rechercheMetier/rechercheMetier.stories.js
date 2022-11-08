cc91e353972b424e5e676297604812eb9f6b8382
H. Pacifique Bayisenge <hpacifique@live.be>
2022-11-08 16:42:55 +0100

    IMR-172-Global-Page-Style-Adapt-correct [skip-ci]

--- ui.frontend/stories/rechercheMetier/rechercheMetier.stories.js
+++ ui.frontend/stories/rechercheMetier/rechercheMetier.stories.js
@@ -1,6 +1,5 @@
 import rechercheMetier from "./rechercheMetier.html";
 import rechercheMetier2 from "./rechercheMetier2.html";
-import rechercheMetierForm from "./rechercheMetierForm.html";
 import { storiesOf } from "@storybook/html";
 
 storiesOf("AEM Components/Recherche Metier", rechercheMetier).add(
@@ -11,9 +10,4 @@ storiesOf("AEM Components/Recherche Metier", rechercheMetier).add(
 storiesOf("AEM Components/Recherche Metier", rechercheMetier2).add(
   "Recherche Metier 2",
   () => rechercheMetier2
-);
-
-storiesOf("AEM Components/Recherche Metier", rechercheMetierForm).add(
-  "Recherche Metier Form",
-  () => rechercheMetierForm
 );
