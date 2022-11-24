60059e4e5d30d79445f09023de107b59562c699a
H. Pacifique Bayisenge <hpacifique@live.be>
2022-11-24 10:01:51 +0100

    IMR-193 popup accessibilité [skip-ci]

--- ui.frontend/src/main/webpack/dependencies/site/js/popup/popup.js
+++ ui.frontend/src/main/webpack/dependencies/site/js/popup/popup.js
@@ -21,13 +21,13 @@ if (popupOpener.length > 0) {
 var popups = document.getElementsByClassName("popup aem");
 if (popups != null) {
   [...popups].forEach((element) => {
-    var closeCross = element.getElementsByClassName("popup-close");
+    var closeCross = element.getElementsByClassName("popup-close-top-btn");
     if (closeCross != null) {
       closeCross[0].addEventListener("click", function (e) {
         closePopup(element);
       });
     }
-    var closeButton = element.getElementsByClassName("closeButton");
+    var closeButton = element.getElementsByClassName("popup-close-bottom-btn");
     if (closeButton != null) {
       closeButton[0].addEventListener("click", function (e) {
         closePopup(element);
