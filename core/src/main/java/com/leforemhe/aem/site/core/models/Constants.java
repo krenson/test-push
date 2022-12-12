982dcb3dbb13916ad7641137fa33880c51d32343
DiegoVanBelle <diego.vanbelle@realdolmen.com>
2022-12-12 11:49:16 +0100

    IMR-229: Removing unused constants

--- core/src/main/java/com/leforemhe/aem/site/core/models/Constants.java
+++ core/src/main/java/com/leforemhe/aem/site/core/models/Constants.java
@@ -5,18 +5,14 @@ public class Constants {
     public static final String CLE_METIER = "clemetier";
     public static final String CLE_METIER_PROPERTY_CF = "jcr:content/clemetier";
     public static final String CONTENT_ROOT_PATH = "/content/leforemhe";
-    public static final String PATH_METIER_CF = "/content/dam/leforemhe/fr/metiers";
     public static final String LAST_UPDATED = "lastUpdatedDate";
     public static final String UPDATE = "update";
 
-    public static final String CQ_TEMPLATE = "cq:template";
     public static final String CQ_PAGE = "cq:Page";
     public static final String CQ_PAGECONTENT = "cq:PageContent";
     public static final String CQ_TAGS = "cq:tags";
-    public static final String CQ_LAST_MODIFIED = "cq:tags";
     public static final String SLING_VANITY_PATH = "sling:vanityPath";
     public static final String DAM_ASSET = "dam:Asset";
-    public static final String SLING_FOLDER = "sling:Folder";
 
     public static final String IMAGE_RENDITION_MARKER = "vig";
     public static final String FALLBACK_RENDITION = "/jcr:content/renditions/cq5dam.web.1280.1280.jpeg";
