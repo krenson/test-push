b395c8922001f5e94672cc243c7a4e4d033100fe
H. Pacifique Bayisenge <hpacifique@live.be>
2022-12-19 16:51:08 +0100

    AEM 1090 List Items displayed as Teasers [skip-ci]

--- ui.frontend/stories/teaserList/teaserList.stories.js
+++ ui.frontend/stories/teaserList/teaserList.stories.js
@@ -1,4 +1,25 @@
-import teaserList from "./teaserList.html";
+import teaserList1Col from "./teaserList1Col.html";
+import teaserList2Col from "./teaserList2Col.html";
+import teaserList3Col from "./teaserList3Col.html";
+import teaserListRow from "./teaserListRow.html";
 import { storiesOf } from "@storybook/html";
 
-storiesOf("AEM Components", teaserList).add("Teaser List", () => teaserList);
+storiesOf("AEM Components/Teaser List", teaserList1Col).add(
+  "Teaser List 1 Column",
+  () => teaserList1Col
+);
+
+storiesOf("AEM Components/Teaser List", teaserList2Col).add(
+  "Teaser List 2 Columns",
+  () => teaserList2Col
+);
+
+storiesOf("AEM Components/Teaser List", teaserList3Col).add(
+  "Teaser List 3 Columns",
+  () => teaserList3Col
+);
+
+storiesOf("AEM Components/Teaser List", teaserListRow).add(
+  "Teaser List Row",
+  () => teaserListRow
+);
