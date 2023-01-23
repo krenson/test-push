0f2be362e53a594ff008ed6cbca2759a749ecf13
pdietger <dietger.pieters@inetum-realdolmen.world>
2023-01-23 16:30:57 +0100

    IMR-283: Implementing changes frontend [skip-ci]

--- ui.apps/src/main/content/jcr_root/apps/leforemhe/components/site/search/clientlibs/js/recherchermetier.js
+++ ui.apps/src/main/content/jcr_root/apps/leforemhe/components/site/search/clientlibs/js/recherchermetier.js
@@ -160,7 +160,7 @@ $(document).ready(function () {
                                     $jobTagHtml.append('<button class="chip-button" style="background-color: ' + jobTag.backgroundColor + '">' + jobTag.title + '</button>');
                                 })
                                 $jobTagHtml.append('</div>');
-                                const html = $(`<div class="teaser tuileContainer">
+                                const html = $(`<div class="teaser tuileContainer" tabindex="0">
 <a href="${result.vanityPath}">
     <div class="cmp-teaser">
         <div class="cmp-teaser__image">
