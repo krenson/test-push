12e41560cac435bc0bc618e760c43d9c2bfe48a1
Jesus Perez <jesus.perez.franco@gfi.be>
2023-01-25 16:11:15 +0100

    AEM-dispatcher Removed dispatcher folder [skip ci]

--- dispatcher.deprecated/dispatcher_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-bin/validator full -d out src
-bin/docker_run.cmd out localhost:4503 test
-bin/docker_run.cmd out host.docker.internal:4503 80 >dispatcher.log 2>&1 &
-
-tail -f dispatcher.log
--- dispatcher.deprecated/dispatcher_stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -rf out
-rm -rf dispatcher.log
-docker ps
-docker stop $(docker ps -aq)
-docker ps
