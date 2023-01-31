c2d6a352466f8247cc6152b9bb2b35b1b8b15441
H. Pacifique Bayisenge <hpacifique@live.be>
2023-01-31 17:46:01 +0100

    IMR-283 tuile focus w keyboard [skip-ci]

--- ui.frontend/src/main/webpack/dependencies/site/js/tuile/tuile.js
+++ ui.frontend/src/main/webpack/dependencies/site/js/tuile/tuile.js
@@ -1,8 +1,34 @@
 try {
-  let tuiles = document.querySelectorAll(".tuileContainer");
+  function dynamicTab() {
+    let tuiles = document.querySelectorAll(".tuileContainer");
 
-  tuiles.forEach((tuile) => {
-    tuile.tabIndex = 0;
-    tuile.querySelector("a").tabIndex = -1;
+    tuiles.forEach((tuile) => {
+      if (tuile.tabIndex == 0) {
+        observer.disconnect();
+        return;
+      }
+      tuile.tabIndex = 0;
+      tuile.querySelector("a").tabIndex = -1;
+    });
+  }
+
+  let observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      let oldValue = mutation.oldValue;
+      let newValue = mutation.target.textContent;
+      if (oldValue !== newValue) {
+        console.log(mutation);
+        dynamicTab();
+      }
+    });
   });
+
+  observer.observe(document.body, {
+    characterDataOldValue: true,
+    subtree: true,
+    childList: true,
+    characterData: true,
+  });
+
+  dynamicTab();
 } catch (error) {}
