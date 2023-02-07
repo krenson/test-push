3ea41dd83650cfcd98f1c3b52631ed9670c145dd
DiegoVanBelle <diego.vanbelle@realdolmen.com>
2023-02-07 12:10:45 +0100

    IMR-268: Fix issue to prevent JS issues.

--- ui.apps/src/main/content/jcr_root/apps/leforemhe/components/site/search/clientlibs/js/request.js
+++ ui.apps/src/main/content/jcr_root/apps/leforemhe/components/site/search/clientlibs/js/request.js
@@ -46,7 +46,7 @@ $(document).ready(function () {
     }
 
     function initSearch() {
-        if (showResults === "true" && dynamicSearch === "false" && searchBtn !== undefined) {
+        if (dynamicSearch === "false" && searchBtn !== undefined) {
             searchBtn.addEventListener("click", doSearch);
         }
     }
