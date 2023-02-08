ff0dd8a286f36db9b88c6be0a02757416484532a
H. Pacifique Bayisenge <hpacifique@live.be>
2023-02-08 14:28:25 +0100

    IMR-283-tuiles focus & tab order

--- ui.frontend/src/main/webpack/dependencies/site/js/tuile/tuile.js
+++ ui.frontend/src/main/webpack/dependencies/site/js/tuile/tuile.js
@@ -1,19 +1,41 @@
-try {
-  // function to make sure the tuile gets a tabindex of 0
-  function dynamicTab() {
-    let tuiles = document.querySelectorAll(".tuileContainer");
+// function to make sure the tuile gets a tabindex of 0
+function dynamicTab() {
+  let tuiles = document.querySelectorAll(".tuileContainer");
 
-    tuiles.forEach((tuile) => {
-      tuile.addEventListener("keypress", (event) => {
-        if (event.key === "Enter") {
-          tuile.querySelector("a").click();
-        }
-      });
-
-      tuile.tabIndex = 0;
-      tuile.querySelector("a").tabIndex = -1;
+  tuiles.forEach((tuile) => {
+    tuile.addEventListener("keypress", (event) => {
+      if (event.key === "Enter") {
+        tuile.querySelector("a").click();
+      }
     });
-  }
 
-  dynamicTab();
-} catch (error) {}
+    if (tuile.tabIndex == 0) {
+      observer.disconnect();
+      return;
+    }
+
+    tuile.tabIndex = 0;
+    tuile.querySelector("a").tabIndex = -1;
+  });
+}
+
+let observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    let oldValue = mutation.oldValue;
+    let newValue = mutation.target.textContent;
+    if (oldValue !== newValue) {
+      dynamicTab();
+      console.log("test");
+    }
+  });
+});
+
+// observe body for changes
+// once api call response 200
+// add tabindex to tuiles
+observer.observe(document.body, {
+  characterDataOldValue: true,
+  subtree: true,
+  childList: true,
+  characterData: true,
+});
