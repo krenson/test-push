56734f7f62d49058595b8c6f97e98b08837394f4
H. Pacifique Bayisenge <hpacifique@live.be>
2023-02-17 10:10:24 +0100

    IMR-300 breadcrumb on mobile spacing [skip-ci]

--- ui.frontend/src/main/webpack/dependencies/site/js/rechercheMetier/rechercheMetier.js
+++ ui.frontend/src/main/webpack/dependencies/site/js/rechercheMetier/rechercheMetier.js
@@ -271,8 +271,6 @@ try {
     dynamicSpacing();
   });
 
-  //TODO: checkbox should keep bottom -4.8rem
-
   function dynamicSpacing() {
     if (searchContainer == null) return;
 
@@ -318,7 +316,7 @@ try {
         // if short recherche metier
         if (shortRecherMetier.className == "short-searchContainer") {
           searchCheckbox.style.bottom = "-8.8rem";
-
+          searchContainer.style.marginBottom = "5rem";
           searchbox.style.marginBottom = "5rem";
         } else {
           // if not short recherche metier
@@ -347,8 +345,14 @@ try {
       }
 
       // to push page content below the mobile recherche metier
-      let marginBottom = ((valueChips.offsetHeight - 88) / 46) * 5 + 18 + 5;
-      form.style.marginBottom = marginBottom + "rem";
+
+      if (
+        shortRecherMetier.className != "short-searchContainer" &&
+        form.style.display != "none"
+      ) {
+        let marginBottom = ((valueChips.offsetHeight - 88) / 46) * 5 + 18 + 5;
+        form.style.marginBottom = marginBottom + "rem";
+      }
     }
 
     if (
