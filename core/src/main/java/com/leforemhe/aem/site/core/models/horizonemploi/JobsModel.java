d4bd8634e829500ab1e5e3c0d995c4476ddff25a
pdietger <dietger.pieters@inetum-realdolmen.world>
2023-02-22 16:24:05 +0100

    IMR-323: Optimized jobs model and added empty list placeholder

--- core/src/main/java/com/leforemhe/aem/site/core/models/horizonemploi/JobsModel.java
+++ core/src/main/java/com/leforemhe/aem/site/core/models/horizonemploi/JobsModel.java
@@ -4,6 +4,7 @@ import com.day.cq.wcm.api.Page;
 import com.leforemhe.aem.site.core.models.Constants;
 import com.leforemhe.aem.site.core.models.cfmodels.Job;
 import com.leforemhe.aem.site.core.services.ContentFragmentUtilService;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
@@ -27,22 +28,34 @@ public class JobsModel {
     @ValueMapValue
     private String selectedJobElement;
 
+    @ValueMapValue
+    private String placeholderEmptyJobsList;
+
     @Self
     private SlingHttpServletRequest request;
 
     private Job job;
+    private List<Job> jobs;
 
     public List<Job> getJobs() {
-        List<Job> jobs = new ArrayList<>();
-        boolean inExperienceFragment = currentPage.getContentResource().getResourceType().equalsIgnoreCase(Constants.EF_RESOURCE_TYPE);
-        if (this.job == null && !inExperienceFragment) {
-            String cleMetier = contentFragmentUtilService.getCleMeteirFromPage(currentPage);
-            this.job = contentFragmentUtilService.getJobFromJobID(cleMetier, true, true, request);
-            if (job != null) {
-                jobs = job.getJobsFromElement(selectedJobElement);
+        if (jobs == null) {
+            jobs = new ArrayList<>();
+            boolean inExperienceFragment = currentPage.getContentResource().getResourceType().equalsIgnoreCase(Constants.EF_RESOURCE_TYPE);
+            if (this.job == null && !inExperienceFragment) {
+                String cleMetier = contentFragmentUtilService.getCleMeteirFromPage(currentPage);
+                this.job = contentFragmentUtilService.getJobFromJobID(cleMetier, true, true, request);
+                if (job != null) {
+                    jobs = job.getJobsFromElement(selectedJobElement);
+                }
             }
+            return jobs;
+        } else {
+            return jobs;
         }
-        return jobs;
+    }
+
+    public String getPlaceholderEmptyJobsList() {
+        return getJobs() != null ? placeholderEmptyJobsList : StringUtils.EMPTY;
     }
 
 }
