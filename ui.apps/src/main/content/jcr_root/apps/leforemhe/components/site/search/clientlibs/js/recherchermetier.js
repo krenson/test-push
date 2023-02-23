72118d205bec497e51e084b7e43b32dccd7b6ef5
pdietger <dietger.pieters@inetum-realdolmen.world>
2023-02-23 09:55:13 +0100

    Sync JS

--- ui.apps/src/main/content/jcr_root/apps/leforemhe/components/site/search/clientlibs/js/recherchermetier.js
+++ ui.apps/src/main/content/jcr_root/apps/leforemhe/components/site/search/clientlibs/js/recherchermetier.js
@@ -344,6 +344,9 @@ $(document).ready(function () {
                 searchContainer.style.bottom = "-10rem";
                 searchbox.style.marginBottom = "0rem";
                 form.style.marginBottom = "0rem";
+                if (shortRecherMetier.className != "short-searchContainer" && form.style.display != "none") {
+                    form.style.marginBottom = "15rem";
+                }
             }
 
             if (window.screen.width > 1200 && suggestions.style.display == "block") {
