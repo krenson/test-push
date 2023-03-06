2211765ed0f24f11bc3ec484e7db83ca92bb6a9c
pdietger <dietger.pieters@inetum-realdolmen.world>
2023-03-06 17:20:43 +0100

    Update develop version

--- core/src/main/java/com/leforemhe/aem/site/core/listeners/package-info.java
+++ core/src/main/java/com/leforemhe/aem/site/core/listeners/package-info.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-@Version("0.0.2")
+@Version("0.0.3")
 package com.leforemhe.aem.site.core.listeners;
 
 import org.osgi.annotation.versioning.Version;
